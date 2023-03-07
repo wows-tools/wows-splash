8dfd3edf4021b060dd5f6643d834037f4cfbeab4
kakwa <carpentier.pf@gmail.com>
2023-03-07 23:23:14 +0100

    definition of the base structs + boilerplate

--- inc/wows-splash.h
+++ inc/wows-splash.h
@@ -1,1 +1,16 @@
-int wows_parse_splash(char *input);
+#include <stdint.h>
+
+typedef struct {
+    uint32_t entry_count;
+    void *entries; // struct *hashmap
+} wows_splash;
+
+typedef struct {
+    uint32_t name_len;
+    char *name;
+    uint8_t data[24]; // 192 bits of data, maybe 3 x 64 bits fields
+} wows_splash_entry;
+
+int wows_parse_splash(char *input, wows_splash **splash_content);
+int wows_splash_print(wows_splash *splash_content);
+int wows_splash_free(wows_splash *splash_content);
